371a4beed3ec7ab801ac88576fd6ee1fa062034b
Lukas Krejci <lkrejci@redhat.com>
2021-11-12 00:55:32 +0100

    Site changes for release of revapi revapi_basic_features revapi_jackson revapi_java_spi revapi_reporter_file_base revapi_ant_task revapi_java revapi_json revapi_reporter_json revapi_reporter_text revapi_standalone revapi_yaml revapi_maven_plugin coverage revapi_examples

---
+++ revapi-jackson/0.4.1/_attachments/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.revapi.jackson","l":"JacksonApiAnalyzer"},{"p":"org.revapi.jackson","l":"JacksonArchiveAnalyzer"},{"p":"org.revapi.jackson","l":"JacksonDifferenceAnalyzer"},{"p":"org.revapi.jackson","l":"JacksonElement"}]
